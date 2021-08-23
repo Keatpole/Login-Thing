2cf90bcc8606f590d4b21790aebdc03f0a153d7b
Isangedal <isaknikolai10@gmail.com>
2021-08-23 13:09:26 +0200

    Added database file

---
+++ .ht-isak_login_thing.sql
@@ -0,0 +1,355 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 23. Aug, 2021 13:07 PM
+-- Tjener-versjon: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `isak_login_thing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `deletedgroupmessages`
+--
+
+CREATE TABLE `deletedgroupmessages` (
+  `id` int(11) NOT NULL,
+  `msgid` int(255) NOT NULL,
+  `message` varchar(250) NOT NULL,
+  `author` int(255) NOT NULL,
+  `replyTo` int(255) NOT NULL,
+  `groupId` int(255) NOT NULL,
+  `createdate` date NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `deletedmessages`
+--
+
+CREATE TABLE `deletedmessages` (
+  `id` int(11) NOT NULL,
+  `msgid` int(255) NOT NULL,
+  `message` varchar(250) NOT NULL,
+  `author` int(255) NOT NULL,
+  `likes` int(255) NOT NULL DEFAULT 0,
+  `replyTo` int(255) NOT NULL,
+  `createdate` date NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `friendreq`
+--
+
+CREATE TABLE `friendreq` (
+  `id` int(11) NOT NULL,
+  `user1` int(11) NOT NULL,
+  `user2` int(11) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `friends`
+--
+
+CREATE TABLE `friends` (
+  `id` int(11) NOT NULL,
+  `user1` int(11) NOT NULL,
+  `user2` int(11) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `groupmessages`
+--
+
+CREATE TABLE `groupmessages` (
+  `id` int(11) NOT NULL,
+  `message` varchar(250) NOT NULL,
+  `author` int(255) NOT NULL,
+  `replyTo` int(255) NOT NULL,
+  `groupId` int(255) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL DEFAULT 'Unnamed',
+  `author` int(255) NOT NULL,
+  `members` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `log`
+--
+
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(250) NOT NULL,
+  `targetsUid` varchar(250) NOT NULL,
+  `action` varchar(128) NOT NULL,
+  `type` varchar(128) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `messagelikes`
+--
+
+CREATE TABLE `messagelikes` (
+  `id` int(11) NOT NULL,
+  `msgid` int(250) NOT NULL,
+  `userid` int(250) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `message` varchar(250) NOT NULL,
+  `author` int(255) NOT NULL,
+  `likes` int(255) NOT NULL DEFAULT 0,
+  `replyTo` int(255) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `modsuggestions`
+--
+
+CREATE TABLE `modsuggestions` (
+  `id` int(11) NOT NULL,
+  `suggester` varchar(255) NOT NULL,
+  `targetsUid` varchar(128) NOT NULL,
+  `type` varchar(128) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `reports`
+--
+
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL,
+  `reporter` int(255) NOT NULL,
+  `target` int(255) NOT NULL,
+  `reason` varchar(128) NOT NULL,
+  `otherreason` text NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `uid` varchar(128) NOT NULL,
+  `pwd` varchar(128) NOT NULL,
+  `rank` int(11) NOT NULL DEFAULT 0,
+  `verified` int(1) NOT NULL DEFAULT 0,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `deletedgroupmessages`
+--
+ALTER TABLE `deletedgroupmessages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `deletedmessages`
+--
+ALTER TABLE `deletedmessages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `friendreq`
+--
+ALTER TABLE `friendreq`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `groupmessages`
+--
+ALTER TABLE `groupmessages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messagelikes`
+--
+ALTER TABLE `messagelikes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `modsuggestions`
+--
+ALTER TABLE `modsuggestions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `deletedgroupmessages`
+--
+ALTER TABLE `deletedgroupmessages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `deletedmessages`
+--
+ALTER TABLE `deletedmessages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `friendreq`
+--
+ALTER TABLE `friendreq`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `friends`
+--
+ALTER TABLE `friends`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `groupmessages`
+--
+ALTER TABLE `groupmessages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messagelikes`
+--
+ALTER TABLE `messagelikes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `modsuggestions`
+--
+ALTER TABLE `modsuggestions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
